3258fcc2b7544912fc74da39ca3dce757b398329
ihsanullahkhan11 <ihsan666880@gmail.com>
2024-09-15 00:57:43 -0700

    comment-remove

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-// var bgColorChange = document.getElementsByClassName("body")[0];
+
 var button = document.querySelector("button")
 button.style.backgroundColor="black";
 button.style.color="white";
 var color = "white";
-// function colreChange (){
-//     if (bgColorChange.classList.contains("Day")) {
-//         bgColorChange.classList.remove("Day");
-//         bgColorChange.classList.add("Night");
-//         button.classList.add("Night")
-//         button.classList.remove("Night")
-//         button.classList.add("Day")
-//         button.textContent = "Day";
-//     } else {
-//         bgColorChange.classList.remove("Night");
-//         bgColorChange.classList.add("Day");
-//         button.classList.remove("Day")
-//         button.classList.add("Night")
-//         button.textContent = "Night";
-//     }
-
-// } 
 
 
 function colreChange(){
